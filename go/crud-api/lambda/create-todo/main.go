76523e43b8125a9c43d89dc511186e540c570e49
akaSomix <somix.land@null.net>
2023-04-09 09:47:07 +0200

    update template to output APIGW link and API Key

--- go/crud-api/lambda/create-todo/main.go
+++ go/crud-api/lambda/create-todo/main.go
@@ -6,11 +6,11 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(event any) (string, error) {
+func CreateTodo(event any) (string, error) {
 	fmt.Printf("Event Received: %s \n", event)
 	return "Hello World", nil
 }
 
 func main() {
-        lambda.Start(Handler)
+        lambda.Start(CreateTodo)
 }
