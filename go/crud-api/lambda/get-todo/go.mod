a1e182278058d9878fc241f361176976bf6d5392
akaSomix <somix.land@null.net>
2023-04-10 22:27:20 +0200

    init get lambda

--- go/crud-api/lambda/get-todo/go.mod
+++ go/crud-api/lambda/get-todo/go.mod
@@ -2,4 +2,11 @@ module github.com/aka-somix/crud-api/get-todo
 
 go 1.20
 
-require github.com/aws/aws-lambda-go v1.39.1
+require (
+	github.com/aka-somix/crud-api/_shared v1.0.0
+	github.com/aws/aws-lambda-go v1.39.1
+)
+
+require github.com/google/uuid v1.3.0 // indirect
+
+replace github.com/aka-somix/crud-api/_shared => ../_shared
--- go/crud-api/lambda/get-todo/main.go
+++ go/crud-api/lambda/get-todo/main.go
@@ -3,14 +3,37 @@ package main
 import (
 	"fmt"
 
+	"github.com/aka-somix/crud-api/_shared/Todo"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(event any) (string, error) {
-	fmt.Printf("Event Received: %s \n", event)
-	return "Hello World", nil
+type GetAllTodosResponseBody struct {
+	Todos []Todo.Todo
+}
+
+type GetTodoResponseBody struct {
+	Todos Todo.Todo
+}
+
+func GetTodo(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Event Received: %v \n", req)
+	
+	fmt.Printf("Path elements: %v \n", req.QueryStringParameters)
+
+	if id, found := req.QueryStringParameters["id"]; found {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body: "Requested ID:" + id,
+		}, nil
+	} else {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 200,
+			Body: "Requested All",
+		}, nil
+	}
 }
 
 func main() {
-        lambda.Start(Handler)
+	lambda.Start(GetTodo)
 }
